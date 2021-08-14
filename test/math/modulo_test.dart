3cbd562b267fb914d562d5ec96ad898815ee1a58
MohamedElmdary <engm5081@gmail.com>
2021-08-14 13:17:41 +0200

    Updated modulo to use %

--- test/math/modulo_test.dart
+++ test/math/modulo_test.dart
@@ -3,9 +3,9 @@ import 'package:ramda/ramda.dart' as R;
 
 void main() {
   test('Should return x % y', () {
-    expect(R.modulo(2, 2), 0);
-    expect(R.modulo(1, 2), 1);
-    expect(R.modulo(-3, 2), -1);
-    expect(R.modulo(-1.5, 2), -1.5);
+    expect(R.modulo(2, 2), 2 % 2);
+    expect(R.modulo(1, 2), 1 % 2);
+    expect(R.modulo(-3, 2), -3 % 2);
+    expect(R.modulo(-1.5, -2), -1.5 % -2);
   });
 }
