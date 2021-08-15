40f303ef898290feaca430a750903d8f4f18b13b
MohamedElmdary <engm5081@gmail.com>
2021-08-15 22:12:08 +0200

    Added more test for countBy

--- test/relation/countBy_test.dart
+++ test/relation/countBy_test.dart
@@ -3,7 +3,10 @@ import 'package:ramda/ramda.dart' as R;
 
 void main() {
   test('Should count by passed fn.', () {
-    final res = R.countBy(R.toUpper, ['a', 'a', 'b', 'c', 'b']);
-    expect(res, {'A': 2, 'B': 2, 'C': 1});
+    final res0 = R.countBy(R.toUpper, ['a', 'a', 'b', 'c', 'b']);
+    expect(res0, {'A': 2, 'B': 2, 'C': 1});
+
+    final res1 = R.countBy(R.floor)([1.5, 1.3, 1.7, 2, 2.5]);
+    expect(res1, {1: 3, 2: 2});
   });
 }
